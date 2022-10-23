bcc89ef297b5da056d7c0535fcd64e63208628a0
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-10-23 15:02:13 +0530

    feat: add FAQ section

--- pages/event/[id].tsx
+++ pages/event/[id].tsx
@@ -5,10 +5,14 @@ import Text from "../../components/Text";
 import { H1, H4, H3, H5, H6 } from "../../components/Headings";
 import Calendar from "../../components/Icons/Calendar";
 import BuyTicketForm from "../../components/BuyTicketForm";
+import MessageChatCircle from "../../components/Icons/MessageChatCircle";
+import { Disclosure } from "@headlessui/react";
+import ChevronDown from "../../components/Icons/ChevronDown";
+import clsx from "clsx";
 
 const Event = () => {
     return (
-        <div className="flex flex-col items-start space-y-8 justify-center px-32 pt-12">
+        <div className="flex flex-col items-start space-y-8 justify-center pl-10 pr-28 pt-12">
             <div className="flex flex-col space-y-2">
                 <H1>Learn from Founders of 9 Figure Online Stores</H1>
                 <div className="flex space-x-2 items-center">
@@ -54,7 +58,113 @@ const Event = () => {
                             </div>
                         </div>
                         <div>
-                            <div>FAQ</div>
+                            <div className="text-2xl font-medium flex mb-6 flex-col space-y-4 text-black">
+                                <H3 className="font-semibold text-gray-700">
+                                    FAQ
+                                </H3>
+                                <div className="flex flex-col items-start">
+                                    <Disclosure>
+                                        {({ open }) => (
+                                            <div
+                                                className={clsx("w-full", {
+                                                    "bg-gray-100": open,
+                                                })}
+                                            >
+                                                <Disclosure.Button
+                                                    className={clsx(
+                                                        "text-left hover:bg-gray-50 p-4 flex justify-between w-full",
+                                                        {
+                                                            "border-b-2 border-gray-200":
+                                                                !open,
+                                                        }
+                                                    )}
+                                                >
+                                                    <H4 className="text-gray-700">
+                                                        When is the event?
+                                                    </H4>
+                                                    <div
+                                                        className={
+                                                            open
+                                                                ? `rotate-180`
+                                                                : ""
+                                                        }
+                                                    >
+                                                        <ChevronDown
+                                                            className={clsx(
+                                                                "stroke-gray-400"
+                                                            )}
+                                                        />
+                                                    </div>
+                                                </Disclosure.Button>
+                                                <Disclosure.Panel
+                                                    className={clsx(
+                                                        "mt-2 pb-4 pl-4 w-full",
+                                                        {
+                                                            "border-b-2 border-gray-200":
+                                                                open,
+                                                        }
+                                                    )}
+                                                >
+                                                    <H5 className="text-gray-700">
+                                                        The event is on 1st
+                                                        January 2023.
+                                                    </H5>
+                                                </Disclosure.Panel>
+                                            </div>
+                                        )}
+                                    </Disclosure>
+                                    <Disclosure>
+                                        {({ open }) => (
+                                            <div
+                                                className={clsx("w-full", {
+                                                    "bg-gray-100": open,
+                                                })}
+                                            >
+                                                <Disclosure.Button
+                                                    className={clsx(
+                                                        "text-left hover:bg-gray-50 p-4 flex justify-between w-full",
+                                                        {
+                                                            "border-b-2 border-gray-200":
+                                                                !open,
+                                                        }
+                                                    )}
+                                                >
+                                                    <H4 className="text-gray-700">
+                                                        When is the event?
+                                                    </H4>
+                                                    <div
+                                                        className={
+                                                            open
+                                                                ? `rotate-180`
+                                                                : ""
+                                                        }
+                                                    >
+                                                        <ChevronDown
+                                                            className={clsx(
+                                                                "stroke-gray-400"
+                                                            )}
+                                                        />
+                                                    </div>
+                                                </Disclosure.Button>
+                                                <Disclosure.Panel
+                                                    className={clsx(
+                                                        "mt-2 pb-4 pl-4 w-full",
+                                                        {
+                                                            "border-b-2 border-gray-200":
+                                                                open,
+                                                        }
+                                                    )}
+                                                >
+                                                    <H5 className="text-gray-700">
+                                                        The event is on 1st
+                                                        January 2023.
+                                                    </H5>
+                                                </Disclosure.Panel>
+                                            </div>
+                                        )}
+                                    </Disclosure>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -86,8 +196,9 @@ const Event = () => {
                             <button className="bg-brand-100 text-brand-700 max-w-fit text-lg border-2 focus:ring-2 focus:ring-brand-100 outline-none ring-offset-2 border-transparent shadow-inner px-5 py-2 w-full h-full rounded-md active:ring-2 active:ring-brand-100">
                                 Share this event
                             </button>
-                            <button className="bg-indigo-600 max-w-fit text-lg border-2 focus:ring-2 focus:ring-indigo-500 outline-none ring-offset-2 border-transparent shadow-md px-5 py-2 w-full h-full rounded-md active:ring-2 active:ring-indigo-500">
-                                Join Discord
+                            <button className="bg-[#7289da] flex space-x-2 max-w-fit text-lg border-2 focus:ring-2 focus:ring-[#7289da] outline-none ring-offset-2 border-transparent shadow-md px-5 py-2 w-full h-full rounded-md active:ring-2 active:ring-[#7289da]">
+                                <MessageChatCircle className="stroke-white" />{" "}
+                                <H4 className="text-white">Join Discord</H4>
                             </button>
                         </div>
                     </div>
