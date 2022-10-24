a4ab5df5b72cd8cb87c04f0f1c0fe9a16f8372ea
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-10-24 20:37:07 +0530

    refactor: add `SidebarMenuItem`

--- components/SidebarMenuItem.tsx
+++ components/SidebarMenuItem.tsx
@@ -8,12 +8,12 @@ const SidebarMenuItem = ({
     itemName: string;
 }) => {
     return (
-        <div className="hover:bg-brand-200 hover:shadow-inner transition-colors duration-150 ease-in-out group w-full flex space-x-2 items-center rounded-md px-3 py-2">
+        <li className="hover:bg-brand-200 hover:shadow-inner transition-colors duration-150 ease-in-out group w-full flex space-x-2 items-center rounded-md px-3 py-2">
             {icon}
             <h4 className="text-lg text-brand-500 transition-colors duration-150 group-hover:text-brand-700 ">
                 {itemName}
             </h4>
-        </div>
+        </li>
     );
 };
 
