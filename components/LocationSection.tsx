3565c9492853bd832c2f405d48b9e35e5ce173bd
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-10-24 20:36:33 +0530

    feat: add `LocationSection`

---
+++ components/LocationSection.tsx
@@ -0,0 +1,23 @@
+import { H5 } from "./Headings";
+import MarkerPin from "./Icons/MarkerPin";
+import Text from "./Text";
+
+const LocationSection = () => {
+    return (
+        <div className="p-4 flex space-x-2 items-start border-brand-100 border-2 rounded-md">
+            <div className="">
+                <MarkerPin />
+            </div>
+            <div className="flex justify-center flex-col space-y-2">
+                <H5 className="text-gray-700 leading-tight">Location</H5>
+                <Text className="text-gray-700 font-medium">
+                    1938 Clove Rd · Staten Island, NY
+                </Text>
+                <Text className="text-sm text-brand-600">
+                    <a href="https://www.google.com">Show on Map</a>
+                </Text>
+            </div>
+        </div>
+    );
+};
+export default LocationSection;
