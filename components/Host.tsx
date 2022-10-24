00d93cdb29adb190b56a6bf4c68889b1fda26443
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-10-24 20:36:22 +0530

    feat: add `Host`

---
+++ components/Host.tsx
@@ -0,0 +1,14 @@
+import { H5, H6 } from "./Headings";
+
+const Host = () => {
+    return (
+        <li>
+            <div className="flex flex-col justify-center">
+                <H5 className="text-purple-500 text-md">Avi from Pesto</H5>
+                <H6 className="text-purple-700 font-semibold text-sm">Host</H6>
+            </div>
+        </li>
+    );
+};
+
+export default Host;
