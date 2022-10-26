58d340af3cc81ab7e770ca01937dd402e4bd924c
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-10-26 19:52:19 +0530

    fix: add `key` to `AttendeesList`

--- components/AttendeesList.tsx
+++ components/AttendeesList.tsx
@@ -5,9 +5,11 @@ import { H5 } from "./Headings";
 const AttendeesList = () => {
     return (
         <ul className="max-w-fit items-center flex space-x-[-15px]">
-            {Array.from(Array(5).keys(), (x) => `z-[${x}]`).map((element) => (
-                <Avatar element={element} />
-            ))}
+            {Array.from(Array(5).keys(), (x) => `z-[${x}]`).map(
+                (element, index) => (
+                    <Avatar element={element} key={index} />
+                )
+            )}
             <li className="!ml-[10px]">
                 <H5 className="text-brand-600 font-medium">+59 More</H5>
             </li>
