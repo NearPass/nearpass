dd23743b51725e48ffc1b65713d5b7d1ca5466ba
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-10-26 19:52:46 +0530

    chore: fix width for `FormInputWrapper`

--- components/FormInputWrapper.tsx
+++ components/FormInputWrapper.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const FormInputWrapper = ({ children }: { children: ReactNode }) => {
-    return <div className="flex flex-col space-y-2">{children}</div>;
+    return <div className="flex w-full flex-col space-y-2">{children}</div>;
 };
 
 export default FormInputWrapper;
