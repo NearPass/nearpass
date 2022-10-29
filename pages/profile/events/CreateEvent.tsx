d10c67f0e4ab10d3ea123184d90e23ab645ff067
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-10-29 16:54:31 +0530

    chore: remove commented code & add thumbnail view

--- pages/profile/events/CreateEvent.tsx
+++ pages/profile/events/CreateEvent.tsx
@@ -31,9 +31,7 @@ const FORM_INITIAL_VALUES = {
     venue: "",
     datetime: "1998-09-15T23:00",
     hostname: "",
-    hostwallet: "",
     hostemail: "",
-    passgraphic: "",
     extraquestions: [],
     telegramgroup: "",
     discordinvitelink: "",
@@ -80,38 +78,16 @@ const CreateEvent = () => {
                             </H3>
                             <CreateEventForm formik={formik} />
                         </div>
-                        {/* <div className="flex flex-col space-y-2">
-                            <div className="text-2xl font-medium  text-black">
-                                <H3 className="font-semibold text-gray-700">
-                                    Event Information
-                                </H3>
-                            </div>
-                            <div className="text-black">
-                                <Text className="text-gray-500">
-                                    Join us for our next Investor pitching
-                                    event. We help Startups test, grow and even
-                                    get funding for their business ideas. With
-                                    our afternoon Workshop you can get help
-                                    shaping and testing your idea. At 6pm you
-                                    can sign up to pitch to our panel of Angel
-                                    VCs, win prizes and a shot at funding. If
-                                    you are not ready for formal pitching, join
-                                    our open 1 minute pitching to test Investor
-                                    interest, get advice and even mentorship.
-                                    More judges, the same prizes and its FREE to
-                                    attend. To PITCH go to bit.ly/nyplooza Our
-                                    Winners have raised over $16 million in the
-                                    past 3 years!
-                                </Text>
-                            </div>
-                        </div>
-                        <div>
-                            <FAQSection />
-                        </div> */}
                     </div>
                 </div>
-                <div className="flex space-y-4 flex-col">
+                <div className="flex space-y-4 flex-col sticky top-10">
                     <H3 className="font-semibold text-gray-700">Preview</H3>
+                    {formik.values.thumbnail && (
+                        <img
+                            src={formik.values.thumbnail}
+                            className="rounded-lg"
+                        />
+                    )}
                     <H3>{formik.values.title}</H3>
                     <H5>{formik.values.description}</H5>
                     {formik.values.datetime && (
@@ -130,19 +106,23 @@ const CreateEvent = () => {
                             </H4>
                         </div>
                     )}
-                    <div className="grid grid-cols-2">
-                        {(formik.values.eventtype || formik.values.venue) && (
-                            <div className="flex flex-col border-2 border-brand-500 p-2 rounded-lg">
+                    <div className="grid grid-cols-2 gap-2">
+                        {(formik.values.eventtype ||
+                            formik.values.venue !== "") && (
+                            <div className="flex flex-col border-2 space-y-2 border-brand-500 p-2 rounded-lg">
                                 <H5>Venue Details</H5>
-                                <H4 className="text-brand-600">
-                                    {formik.values.eventtype}
-                                </H4>
+
                                 {formik.values.eventtype === "irl" && (
                                     <div className="flex items-center space-x-2">
                                         <MarkerPin />
                                         <H4 className="text-brand-600 leading-tight">
                                             {formik.values.venue}
                                         </H4>
+                                    </div>
+                                )}
+                                {formik.values.eventtype === "virtual" && (
+                                    <div className="flex items-center space-x-2">
+                                        <H4>Virtual</H4>
                                     </div>
                                 )}
                             </div>
@@ -335,38 +315,6 @@ const CreateEvent = () => {
                             </div>
                         </section>
                     )}
-                    {/* <div className="flex flex-col space-y-4 sticky top-10">
-                        <img
-                            className="rounded-lg"
-                            src="https://secure-content.meetupstatic.com/images/classic-events/507570333/530x298.webp"
-                        />
-                        <div className="flex flex-col space-y-4">
-                            <div className="grid grid-cols-2 gap-2">
-                                <div className="flex hover:bg-brand-100 hover:border-brand-600 items-center justify-between max-w-fit space-x-4 border-2 border-brand-100 p-4 rounded-md">
-                                    <img
-                                        className="rounded-full h-14 w-14"
-                                        src="https://cdn.lu.ma/cdn-cgi/image/format=auto,fit=cover,dpr=2,quality=85,width=40,height=40/avatars/3b/df87ba2b-f455-4f41-ba3b-814721f505f1"
-                                    />
-                                    <ul className="flex flex-1 justify-end space-x-4 w-full">
-                                        <Host />
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="grid grid-cols-2 gap-2">
-                                <LocationSection />
-                                <AttendeesSection />
-                            </div>
-                            <div className="flex space-x-2">
-                                <button className="bg-brand-100 text-brand-700 max-w-fit text-lg border-2 focus:ring-2 focus:ring-brand-100 outline-none ring-offset-2 border-transparent shadow-inner px-5 py-2 w-full h-full rounded-md active:ring-2 active:ring-brand-100">
-                                    Share this event
-                                </button>
-                                <button className="bg-[#7289da] flex space-x-2 max-w-fit text-lg border-2 focus:ring-2 focus:ring-[#7289da] outline-none ring-offset-2 border-transparent shadow-md px-5 py-2 w-full h-full rounded-md active:ring-2 active:ring-[#7289da]">
-                                    <MessageChatCircle className="!stroke-white" />
-                                    <H4 className="text-white">Join Discord</H4>
-                                </button>
-                            </div>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </div>
