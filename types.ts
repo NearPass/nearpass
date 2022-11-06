71e6ae9599bd70da7f8103fcf3cffeeefc734e63
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-06 14:39:03 +0530

    feat: add properties to `Event` type

--- types.ts
+++ types.ts
@@ -1,4 +1,18 @@
 export interface Event {
+    id: string;
+    title: string;
+    price: string;
+    active: boolean;
+    description: string;
+    timestamp: string;
+    eventType: string;
+    venue?: string;
+    hostname: string;
+    image: string;
+    telegram?: string;
+    discord?: string;
+    host: Host;
+}
 export interface Host {
     name: string;
     address: string;
