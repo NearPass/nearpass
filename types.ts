956f33d61bfc4f3cf9ca607d2c49fde07901a996
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-06 19:01:30 +0530

    feat: add `FAQ` type

--- types.ts
+++ types.ts
@@ -8,10 +8,20 @@ export interface Event {
     eventType: string;
     venue?: string;
     hostname: string;
-    image: string;
+    thumbnail: string;
     telegram?: string;
     discord?: string;
     host: Host;
+    faq: FAQ;
+    question1: string;
+    question2: string;
+}
+
+export interface FAQ {
+    question1: string;
+    answer1: string;
+    question2: string;
+    answer2: string;
 }
 
 export interface Host {
