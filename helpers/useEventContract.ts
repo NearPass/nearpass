6aaa1f7f3fab95a7c94179ce6978a6462d3a5beb
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-06 14:44:21 +0530

    chore: update `createEventOnChain` function params

--- helpers/useEventContract.ts
+++ helpers/useEventContract.ts
@@ -1,8 +1,9 @@
 import { Account, Contract, utils } from "near-api-js";
 import { useEffect, useState } from "react";
 import useWallet from "./useWallet";
+import { timestampToDateTime } from "./utils";
 
-const EVENTS_CONTRACT = "dev-1667138638377-68355460764535";
+const EVENTS_CONTRACT = process.env.NEXT_PUBLIC_EVENT_CONTRACT_ADDRESS;
 
 function titleToEventId(title: string) {
     let titleSplit = title.split(" ");
@@ -24,23 +25,33 @@ function useEventContract() {
         let account = await near?.account(
             walletConnection?.getAccountId() as string
         );
-        const contract = new Contract(account as Account, EVENTS_CONTRACT, {
-            changeMethods: ["createEvent"],
-            viewMethods: [],
-        });
+        const contract = new Contract(
+            account as Account,
+            EVENTS_CONTRACT as string,
+            {
+                changeMethods: ["createEvent"],
+                viewMethods: [],
+            }
+        );
         return contract;
     }
 
-    async function createEventOnChain({ title, hostName, price }, ipfsResult) {
+    async function createEventOnChain({
+        title,
+        hostName,
+        price,
+        timestamp,
+        eventMetadata,
+    }) {
         let eventContract = await prepareEventContract();
         const tx = await eventContract.createEvent({
             args: {
                 eventId: titleToEventId(title),
                 title,
-                eventMetadataUrl: ipfsResult.url,
-                eventStart: Date.now(),
+                eventMetadata,
+                eventStart: timestamp,
                 hostName,
-                price: utils.format.formatNearAmount(price.toString()),
+                price: utils.format.parseNearAmount(price.toString()),
             },
         });
         console.log(tx);
