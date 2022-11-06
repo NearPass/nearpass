ee9dd8cc070cc7014f8f0a64b3399cd89151da68
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-06 14:44:38 +0530

    chore: add `upcoming events` link in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import useWallet from "../helpers/useWallet";
 import { WalletConnection, utils } from "near-api-js";
 import { H4, H5, H6 } from "./Headings";
 import AccountDetails from "./AccountDetails";
+import Link from "next/link";
 
 const Sidebar = () => {
     let [_, walletConnection] = useWallet();
@@ -37,12 +38,18 @@ const Sidebar = () => {
     return (
         <div className="flex flex-col justify-between h-full border-r-2 border-gray-200 min-w-[270px] bg-white">
             <div>
-                <div className="text-emerald-800 p-5"></div>
+                <div className="text-emerald-800 p-5">
+                    <img src="/logo.png" className="bg-transparent h-14" />
+                </div>
                 <ul className="flex flex-col space-y-2 p-3 text-emerald-700">
-                    <SidebarMenuItem
-                        icon={<ZapIcon />}
-                        itemName="Upcoming Events"
-                    />
+                    <Link href="/">
+                        <a>
+                            <SidebarMenuItem
+                                icon={<ZapIcon />}
+                                itemName="Upcoming Events"
+                            />
+                        </a>
+                    </Link>
                     <SidebarMenuItem
                         icon={<CheckCircle />}
                         itemName="Past Events"
