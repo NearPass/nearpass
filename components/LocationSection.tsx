c6bc444ac566a3478bddb179d7109faf73df3d89
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-06 14:40:55 +0530

    feat: add `Location` component to show location from Graph

--- components/LocationSection.tsx
+++ components/LocationSection.tsx
@@ -2,7 +2,7 @@ import { H5 } from "./Headings";
 import MarkerPin from "./Icons/MarkerPin";
 import Text from "./Text";
 
-const LocationSection = () => {
+const LocationSection = ({ venue }: { venue: string }) => {
     return (
         <div className="p-4 flex space-x-2 items-start border-brand-100 border-2 rounded-md">
             <div className="">
@@ -10,9 +10,7 @@ const LocationSection = () => {
             </div>
             <div className="flex justify-center flex-col space-y-2">
                 <H5 className="text-gray-700 leading-tight">Location</H5>
-                <Text className="text-gray-700 font-medium">
-                    1938 Clove Rd · Staten Island, NY
-                </Text>
+                <Text className="text-gray-700 font-medium">{venue}</Text>
                 <Text className="text-sm text-brand-600">
                     <a href="https://www.google.com">Show on Map</a>
                 </Text>
