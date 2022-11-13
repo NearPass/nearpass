0d4b24b6971941a0dc1ebc1ea2f6fe6f325e0845
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-13 13:02:15 +0530

    feat: add google maps to `LocationSection`

--- components/LocationSection.tsx
+++ components/LocationSection.tsx
@@ -4,15 +4,20 @@ import Text from "./Text";
 
 const LocationSection = ({ venue }: { venue: string }) => {
     return (
-        <div className="p-4 flex space-x-2 items-start border-brand-100 border-2 rounded-md">
+        <div className="p-4 flex hover:bg-brand-100 hover:border-brand-600 space-x-2 items-start border-brand-100 border-2 rounded-md">
             <div className="">
                 <MarkerPin />
             </div>
-            <div className="flex justify-center flex-col space-y-2">
+            <div className="flex justify-center flex-col">
                 <H5 className="text-gray-700 leading-tight">Location</H5>
                 <Text className="text-gray-700 font-medium">{venue}</Text>
                 <Text className="text-sm text-brand-600">
-                    <a href="https://www.google.com">Show on Map</a>
+                    <a
+                        href={`https://www.google.com/maps/search/${venue}/@20.7710884,73.7280409`}
+                        target="_blank"
+                    >
+                        Show on Map
+                    </a>
                 </Text>
             </div>
         </div>
