ee17c9a69bef628dd29af10a48c5ca933d8e5c79
Harpal Jadeja <jadejaharpal14@gmail.com>
2022-11-13 13:03:54 +0530

    feat: add empty state for Tickets

--- pages/profile/tickets.tsx
+++ pages/profile/tickets.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { H1, H2, H4 } from "../../components/Headings";
+import React, { useEffect, useState } from "react";
+import { H1, H2, H3, H4 } from "../../components/Headings";
 import PrivateRoute from "../../components/PrivateRoute";
 import axios from "axios";
 import useWallet from "../../helpers/useWallet";
@@ -50,13 +50,25 @@ const Tickets = () => {
     return (
         <PrivateRoute>
             <div className="flex space-y-8 flex-1 p-5 flex-col">
-                <div className="flex flex-col space-y-2">
-                    <H1>Tickets</H1>
-                </div>
-                <div className="grid gap-2 grid-cols-4">
-                    {tickets &&
-                        tickets.map((ticket) => <TicketComp ticket={ticket} />)}
-                </div>
+                {tickets && tickets.length <= 0 ? (
+                    <div className="flex items-center justify-center p-4">
+                        <div className="border-2 border-gray-200 w-full rounded-md flex items-center justify-center p-4">
+                            <H3>No Tickets</H3>
+                        </div>
+                    </div>
+                ) : (
+                    <React.Fragment>
+                        <div className="flex flex-col space-y-2">
+                            <H1>Tickets</H1>
+                        </div>
+                        <div className="grid gap-2 grid-cols-4">
+                            {tickets &&
+                                tickets.map((ticket) => (
+                                    <TicketComp ticket={ticket} />
+                                ))}
+                        </div>
+                    </React.Fragment>
+                )}
             </div>
         </PrivateRoute>
     );
